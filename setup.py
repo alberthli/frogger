1e8b56bdef763c173d0a88a553b99d030f7f6f87
alberthli <alberthli@berkeley.edu>
2023-03-10 10:03:30 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="grasp_refinement",
+    name="frogger",
     version="0.0.1",
     author="Albert H. Li",
     author_email="alberthli@caltech.edu",
     description="FRoGGeR: Fast Robust Grasp Generation via the Min-Weight Metric.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alberthli/grasp_refinement",
+    url="https://github.com/alberthli/frogger",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
